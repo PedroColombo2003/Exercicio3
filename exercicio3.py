91d29c5e5055609c17ec5436816debccba759b0f
PedroColombo2003 <pedrocorrea087@gmail.com>
2025-02-25 20:22:58 -0300

    Add files via upload

---
+++ exercicio3.py
@@ -0,0 +1,29 @@
+"""
+Titulo: Exercicio 3
+Autor: Pedro Colombo
+Data: 25/02/2025
+Versão: 0.0.1
+
+"""
+
+# Alocação de memória
+
+a = 0
+
+b = 0
+
+
+# Entrada de dados
+numero_1 = int(input("Escolha um número: "))
+
+numero_2 = int(input("Escolha outro número: "))
+
+
+# Processamento de dados
+
+soma = numero_1 + numero_2
+
+
+#Saida de dados
+
+print(f"o resultado da soma desses dois números é:{soma}")
